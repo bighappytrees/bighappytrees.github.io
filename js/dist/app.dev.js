1579a0e123603f1ed3187503310ba56a7bd7a420
Aaron Bendersky <cryptotrees@gmail.com>
2020-08-23 12:19:32 -0400

    Implement dark mode toggle

---
+++ js/dist/app.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var moonPath = 'M13 27.5C13 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 13 12.3122 13 27.5Z';
+var sunPath = 'M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z';
+var darkMode = document.querySelector('#darkMode');
+var helpText = document.querySelector('#helpText');
+var toggle = false; // click on the sun
+
+darkMode.addEventListener('click', function () {
+  // setup the timeline
+  var timeline = anime.timeline({
+    duration: 750,
+    easing: 'easeOutExpo'
+  });
+  timeline.add({
+    targets: '.sun',
+    d: [{
+      value: toggle ? moonPath : sunPath
+    }]
+  }).add({
+    targets: '#darkMode',
+    rotate: '1turn'
+  }, '-=600').add({
+    targets: 'section',
+    backgroundColor: toggle ? 'rgb(255,255,255)' : 'rgb(22,22,22)'
+  }, '-=700'); // toggle logic
+
+  if (!toggle) {
+    toggle = true;
+  } else {
+    toggle = false;
+  }
+});
